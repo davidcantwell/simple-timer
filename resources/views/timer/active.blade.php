ed6af11a1c2673f29ce807315ed2bedd1f11812d
David Cantwell <david.cantwell@gmail.com>
2024-09-10 11:37:29 +0200

    Add 30s reload to timer page JUST in case something is slow with desktop and Javascript goes wonky.

--- resources/views/timer/active.blade.php
+++ resources/views/timer/active.blade.php
@@ -9,4 +9,10 @@
     <script src="assets/js/jquery-3.7.1.min.js"></script>
     <script src="assets/js/TweenMax.min.js"></script>
     <script src="assets/js/count.js"></script>
+
+    <script>
+        window.setTimeout( function() {
+            window.location.reload();
+        }, 10000);
+    </script>
 @endsection
